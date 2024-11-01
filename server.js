7eee36d15bd3535c4b4308a21756af4c1441268d
Roman <roman.sleboda@yahoo.com>
2024-11-01 08:01:25 +0100

    0000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const dev = process.env.NODE_ENV !== "production"
-const hostname = "localhost"
+const hostname = "0.0.0.0"
 const port = 3001
 
 const app = next({
